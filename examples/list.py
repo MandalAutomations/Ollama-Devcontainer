ce20ce725ab907a0fa9537979a931c93754fcf89
Evan Allen <evanallen13@github.com>
2025-09-14 23:41:41 +0000

    add modefile create

--- examples/list.py
+++ examples/list.py
@@ -7,9 +7,10 @@ OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://ollama:11434")
 EMBEDDING_MODEL = "granite-embedding:30m" # Find available models here https://ollama.com/library
 
 if __name__ == "__main__":
-    llama = llama(OLLAMA_HOST, embedding_model=EMBEDDING_MODEL)
-
+    llama = llama(OLLAMA_HOST)
+    llama.remove_model("tinyllama:1.1b")
     print(llama.get_all_models())
+    
     
     # llama.remove_all_models()
     
